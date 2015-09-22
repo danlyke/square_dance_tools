42832eac4b28c14e847094bbcdb856c52d9787e6
Dan Lyke <danlyke@flutterby.com>
2015-09-22 07:33:27 -0700

    Writing and outputting for the version on the laptop

--- rewrite_for_sd.pl
+++ rewrite_for_sd.pl
@@ -20,38 +20,38 @@ sub get_next_sequence
     
     my $line;
 
-    my $headregex = "($dows)\\s+($months)\\s+(\\d+)\\s+(\\d+\\:\\d+\\:\\d+)\\s+(\\d+)";
+    my $headregex = "($dows)\\s+($months)\\s+(\\d+)\\s+(\\d+\\:\\d+\\:\\d+)\\s+(\\d+)\\s+Sd[^\\s]*\\s+(.*?)(\\s|\$)";
     while (defined($line = <$fh>) && $line !~ /\x0c/
           && $line !~ /$headregex/)
     {
-        print "$line didn't match dows\n";
     }
 
-    return unless $line =~ /$headregex/;
-    print "($1 && $2 && $3 && $4 && $5)\n";
+    return unless defined($line) && $line =~ /$headregex/;
         
     my $sequence =
     {
      date => sprintf('%4.4d-%2.2d-%2.2d %s', $5, $months{$2}, $3, $4),
+     level => $6,
      moves => [],
     };
     
-    return undef unless (defined($line = <$fh>) && $line !~ /\x0c/);
+    return undef unless (defined($line = <$fh>) || $line =~ /\x0c/);
     $line =~ s/^\s+//xsg;
     $line =~ s/\s+$//xsg;
     $sequence->{description} = $line;
     
     while (defined($line = <$fh>) && $line !~ /\x0c/)
     {
+        chomp $line;
         unless ($line =~ /^\s/xs)
         {
             unless (defined($sequence->{opening}))
             {
-                if ($line =~ s/^(heads|sides\s+1p2p)\s*$//xs)
+                if ($line =~ s/^((heads|sides)\s+1p2p)\s*$//xsi)
                 {
                     $sequence->{opening} = $1;
                 }
-                elsif ($line =~ s/^(heads|sides)\s+//xs)
+                elsif ($line =~ s/^(heads|sides)\s+//xsi)
                 {
                     $sequence->{opening} = "$1 start";
                 }
@@ -62,6 +62,8 @@ sub get_next_sequence
             }
         }
     }
+    $sequence->{resolve} = pop @{ $sequence->{moves} };
+    return $sequence;
 }
 
 for my $filename (@ARGV)
@@ -77,6 +79,23 @@ for my $filename (@ARGV)
     }
 
     close $fh;
-
     print Dumper(\@sequences);
+
+    open my $ofh, '|-', './sdtty -sequence test.txt -keep_all_pictures -no_graphics -no_color'
+        || die "unable to open for writing\n";
+    print $ofh "0\n";
+    print $ofh "$sequences[0]->{level}\n";
+    for my $sequence (@sequences)
+    {
+        print $ofh "$sequence->{opening}\n";
+
+        for my $move (@{ $sequence->{moves} })
+        {
+            print $ofh "$move\n";
+        }
+        print $ofh "write this sequence\n";
+        print $ofh "$sequence->{description}\n";
+    }
+    print $ofh "exit\n";
+    close $ofh;
 }
